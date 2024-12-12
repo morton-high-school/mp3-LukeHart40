3bb71db787031d1f4f3a05c154469a1ea340c776
LukeHart40 <lucas.hart@mcusd709.org>
2024-12-12 09:26:20 -0600

    Final Changes
    All tasks completed

--- Student.java
+++ Student.java
@@ -6,6 +6,8 @@ public class Student{
     private static int students;
     private static int Grade;
     private static double points;
+    private String teacherName;
+    private String teacherReason;
 
     public Student(String name){
         this.name = name;
@@ -29,9 +31,14 @@ public class Student{
 
     public void setFavoriteTeacher(Teacher teach){
         favoriteTeacher = teach;
+        teacherName = teach.getName();
+        teacherReason = teach.getReason();
+
     }
 
     public Teacher getFavoriteTeacher(){
+        favoriteTeacher.setName(teacherName);
+        favoriteTeacher.setReason(teacherReason);
         return favoriteTeacher;
     }
 
--- Teacher.java
+++ Teacher.java
@@ -1,29 +1,29 @@
 public class Teacher{
-    public String name;
-    public String reason;
+    private String teacherName;
+    private String teacherReason;
 
     public Teacher(String name, String reason){
-        this.name = name;
-        this.reason = reason;
+        this.teacherName = name;
+        this.teacherReason = reason;
     }
 
     //Getters
 
     public String getName(){
-        return name;
+        return teacherName;
     }
 
     public String getReason(){
-        return reason;
+        return teacherReason;
     }
 
     //Setters
 
     public void setName(String name){
-        this.name = name;
+        this.teacherName = name;
     }
 
     public void setReason(String reason){
-        this.reason = reason;
+        this.teacherReason = reason;
     }
 }
